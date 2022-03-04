40b0f58c0bd711e44201c5cb140bd3d29982b9d8
tenznz <dhugkar@gmail.com>
2022-03-04 12:07:24 +0530

    Added: Table to print details

--- fundooNotes/fundooNotes/urls.py
+++ fundooNotes/fundooNotes/urls.py
@@ -41,6 +41,6 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include("user.urls")),
-    # path('notes/', include("notes.urls")),
+    path('notes/', include("notes.urls")),
     # path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
